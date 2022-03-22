0ef193d6649f21d197e6863195a779527ac3fbee
dymafr <erwan1309@hotmail.com>
2022-03-22 16:26:45 +0100

    1 file modified

--- src/Liste.vue
+++ src/Liste.vue
@@ -11,6 +11,7 @@ import { reactive } from 'vue';
 
 const state = reactive<{ users: any[] }>({ users: [] });
 
+// Délai de 3secondes grâce à l'option delay de restapi
 state.users = await (
   await fetch('https://restapi.fr/api/vueusers?delay=3')
 ).json();
