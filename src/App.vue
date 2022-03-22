83de6e13d5a679949155b4a0aa506f47fc795fef
dymafr <erwan1309@hotmail.com>
2022-03-22 16:33:10 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <h2>{{ evenement }}</h2>
   <Suspense
     @pending="myEvent('PENDING')"
     @fallback="myEvent('FALLBACK')"
@@ -12,12 +13,14 @@
 </template>
 
 <script setup lang="ts">
-import { defineAsyncComponent } from 'vue';
+import { defineAsyncComponent, ref } from 'vue';
 
+const evenement = ref('');
 const LazyList = defineAsyncComponent(() => import('./Liste.vue'));
 
 function myEvent(eventName: string) {
   console.log(eventName);
+  evenement.value = eventName;
 }
 </script>
 
